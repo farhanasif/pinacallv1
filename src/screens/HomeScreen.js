38b1a137570e44f9b77b44f1fb15a5d3f71a52ab
Farhan Asif <f.asif2010@gmail.com>
2020-11-13 01:07:33 +0600

    another home making

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { View, Text, Button, TouchableOpacity, Animated, StyleSheet } from 'react-native';
+import { View, Text, Button, TouchableOpacity, Animated, StyleSheet, Image } from 'react-native';
 import { COLORS } from '../assets/utils/colors';
 import { Appbar } from 'react-native-paper';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -47,124 +47,36 @@ export default function HomeScreen({navigation}) {
 
   return (
     <View style={{ flex: 1,}}>
-      <LinearGradient colors={['#E7412B','#F50B5E', '#CF005F', '#9A0F3C']} start={[0.0, 0.5]} end={[1.0, 0.5]} >
-        <Appbar.Header style={{paddingLeft: 15, backgroundColor: 'transparent'}}>
-          <Appbar.Content title="Pinacall" subtitle="Pinacall Subtitle" color="#FFF"/>
-          <Appbar.Action icon="magnify" onPress={_handleSearch} color="#FFF"/>
-          <Appbar.Action icon="dots-horizontal-circle" onPress={_handleMore} color="#FFF"/>
-        </Appbar.Header>
-      </LinearGradient>
+      <View style={styles.header}>
+        <Image
+          style={{height: 140, width: 140}}
+          source={require('../assets/images/pinacall_final_01.png')}
+        />
+      </View>
       
       <View>
         <View style={{ alignItems: 'flex-start', paddingLeft: 20, paddingTop: 20 }}>
           <Text style={{ fontSize: 22, fontWeight: '700'}}>Welcome Farhan,</Text>
           <Text style={{ fontWeight: '300', color: '#414141'}}>Choose the type of service you are interested</Text>
         </View>
-        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', paddingTop: 12}}>
-          <Animated.View style={{...styles.box, ...animatedStyle}} >
-            <MaterialCommunityIcons name="phone" size={55} color="white" />
-            <Text style={{marginTop: 40, fontSize: 14, fontWeight: '700', color: '#FFF'}}>PIN A CALL</Text>
-          </Animated.View>
-          <View style={{
-            borderColor: COLORS.pinacall_middle,
-            borderWidth: 3,
-            width: windowWidth/2 - 30,
-            borderRadius: 10,
-            height: 170,
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: COLORS.pinacall_orange,
-            shadowColor: "#000",
-            shadowOffset: {
-              width: 0,
-              height: 5,
-            },
-            shadowOpacity: 0.34,
-            shadowRadius: 6.27,
-            elevation: 10,
-          }}>
-            <FontAwesome5 name="shopping-bag" size={55} color="white" />
-            <Text style={{marginTop: 40, fontSize: 14, fontWeight: '700', color: '#FFF'}}>WINDOW SHOPPING</Text>
-          </View>
+        <View style={{ alignItems: 'flex-start', paddingLeft: 20, paddingTop: 20 }}>
+          <Text style={{ fontSize: 22, fontWeight: '700'}}>DISCOVER PINACALL</Text>
+          <Text style={{ fontWeight: '300', color: '#414141'}}>What you are looking for.</Text>
         </View>
-        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', paddingTop: 30}}>
-          <View style={{
-            borderColor: COLORS.orange2,
-            borderWidth: 3,
-            width: windowWidth/2 - 30,
-            borderRadius: 10,
-            height: 170,
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: COLORS.orange2,
-            shadowColor: "#000",
-            shadowOffset: {
-              width: 0,
-              height: 5,
-            },
-            shadowOpacity: 0.34,
-            shadowRadius: 6.27,
-
-            elevation: 10,
-          }}>
-            <FontAwesome5 name="hotel" size={55} color="white" />
-            <Text style={{marginTop: 40, fontSize: 14, fontWeight: '700', color: '#FFF'}}>HOTEL BOOKING</Text>
-          </View>
-          <View style={{
-            borderColor: COLORS.pinacall_pink,
-            borderWidth: 3,
-            width: windowWidth/2 - 30,
-            borderRadius: 10,
-            height: 170,
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: COLORS.pinacall_pink,
-            shadowColor: "#000",
-            shadowOffset: {
-              width: 0,
-              height: 5,
-            },
-            shadowOpacity: 0.34,
-            shadowRadius: 6.27,
-
-            elevation: 10,
-          }}>
-            <MaterialCommunityIcons name="account-box-outline" size={55} color="white" />
-            <Text style={{marginTop: 40, fontSize: 14, fontWeight: '700', color: '#FFF'}}>EXPERT ADVISE</Text>
-          </View>
-        </View>
-        <View style={{ flexDirection: 'row', alignItems: 'flex-start', paddingTop: 30, paddingLeft: 18}}>
-          <View style={{
-            borderColor: COLORS.blue,
-            borderWidth: 3,
-            width: windowWidth/2 - 30,
-            borderRadius: 10,
-            height: 170,
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: COLORS.blue,
-            shadowColor: "#000",
-            shadowOffset: {
-              width: 0,
-              height: 5,
-            },
-            shadowOpacity: 0.34,
-            shadowRadius: 6.27,
-
-            elevation: 10,
-          }}>
-            <MaterialCommunityIcons name="eye" size={55} color="white" />
-            <Text style={{marginTop: 40, fontSize: 14, fontWeight: '700', color: '#FFF'}}>3RD EYE</Text>
-          </View>
+        <View>
           
         </View>
       </View>
-      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  header:{
+    paddingTop: 40,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },  
   box: {
     borderWidth: 0,
     width: windowWidth/2 - 30,
